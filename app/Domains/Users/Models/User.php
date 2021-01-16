fac8ea4291c6a80fce86d002fc64e2678d0c370f
Lucas Formiga <lucasformiga@gmail.com>
2021-01-16 15:58:31 -0300

    Installing Laravel Sanctum

--- app/Domains/Users/Models/User.php
+++ app/Domains/Users/Models/User.php
@@ -8,13 +8,15 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
+    use HasApiTokens;
     use HasFactory;
     use Notifiable;
-    use UuidIncrements;
     use SoftDeletes;
+    use UuidIncrements;
 
     protected $keyType = 'string';
     public $incrementing = false;
